0f6e1d88d01810c2f29fc783a1b56b32e01687a6
Tornike Janashia <t.janashia11@gmail.com>
2020-06-06 01:47:20 +0400

    feat(part3): add 3.22: Lint configuration exercise

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 app.use(express.static('build'))
-app.use(express.json());
+app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 
@@ -11,125 +11,103 @@ const Person=require('./models/person')
 var morgan = require('morgan')
 
 app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-    ].join(' ') + ' '+ JSON.stringify(req.body)
-  }));
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+  ].join(' ') + ' '+ JSON.stringify(req.body)
+}))
 
-
-// let persons=[
-//       {
-//         "name": "Arto Hellas",
-//         "number": "o23502350",
-//         "id": 1
-//       },
-//       {
-//         "name": "Ada Lovelace",
-//         "number": "39-44-5323523",
-//         "id": 2
-//       },
-//       {
-//         "name": "Dan Abramov",
-//         "number": "12-43-234345",
-//         "id": 3
-//       },
-//       {
-//         "name": "Mary Poppendieck",
-//         "number": "39-23-6423122",
-//         "id": 4
-//       }
-//     ];
 
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (req, res) => {
-    Person.find({}).then(result =>{
-        res.send(
-            `
+  Person.find({}).then(result => {
+    res.send(
+      `
             <p>Phonebook has info for ${result.length} people</p>
             <p>${new Date()}</p>
             `
-        )
-    })
+    )
+  })
 
 })
 
 
-app.get('/api/persons',(req,res)=>{
-    Person.find({}).then(result =>{
-        console.log(result)
-        res.json(result);
-    })
-});
+app.get('/api/persons',(req,res) => {
+  Person.find({}).then(result => {
+    console.log(result)
+    res.json(result)
+  })
+})
 
-app.post('/api/persons',(req,res, next)=>{
-    const person=req.body;
+app.post('/api/persons',(req,res, next) => {
+  const person=req.body
 
-    const personDb=new Person({
-        name:person.name,
-        number:person.number
-    });
+  const personDb=new Person({
+    name:person.name,
+    number:person.number
+  })
 
-    personDb.save().then(savedPerson =>{
-        res.json(savedPerson.toJSON());
-    })
+  personDb.save().then(savedPerson => {
+    res.json(savedPerson.toJSON())
+  })
     .catch(error => next(error))
-});
+})
 
-app.get('/api/persons/:id',(req,res,next)=>{
-    const id=req.params.id;
-    Person.findById(id)
-        .then(person=>{
-            if(person){
-                res.json(person)
-            } else{
-                res.status(404).end()
-            }
-        })
-        .catch(error => {
-            next(error);
-        })
-});
+app.get('/api/persons/:id',(req,res,next) => {
+  const id=req.params.id
+  Person.findById(id)
+    .then(person => {
+      if(person){
+        res.json(person)
+      } else{
+        res.status(404).end()
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
+})
 
-app.put('/api/persons/:id',(req,res,next)=>{
-    const number=req.body.number;
-    Person.findByIdAndUpdate(req.param.id,{number:number},{ new: true })
+app.put('/api/persons/:id',(req,res,next) => {
+  const number=req.body.number
+  Person.findByIdAndUpdate(req.param.id,{ number:number },{ new: true })
     .then(updatedPerson => {
-        res.json(updatedPerson)
-      })
+      res.json(updatedPerson)
+    })
     .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id',(req,res, next)=>{
-    Person.findByIdAndRemove(req.params.id)
-    .then(result => {
+app.delete('/api/persons/:id',(req,res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
       res.status(204).end()
     })
     .catch(error => next(error))
-});
+})
 
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
-    }
-  
-    next(error)
   }
-  
-  app.use(errorHandler)
 
-const PORT = process.env.PORT || 3001;
+  next(error)
+}
 
-app.listen(PORT, '0.0.0.0');
+app.use(errorHandler)
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, '0.0.0.0')
--- models/person.js
+++ models/person.js
@@ -1,10 +1,11 @@
 
-const mongoose=require('mongoose');
-const  uniqueValidator = require('mongoose-unique-validator');
+const mongoose=require('mongoose')
+const  uniqueValidator = require('mongoose-unique-validator')
+// eslint-disable-next-line no-undef
 const url =process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -12,21 +13,21 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   })
 
 const personSchema = new mongoose.Schema({
-  name: {type:String, minlength:3, required:true, unique:true},
-  number: {type:String, minlength:8, required:true},
+  name: { type:String, minlength:3, required:true, unique:true },
+  number: { type:String, minlength:8, required:true },
 })
 
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-  
+
 mongoose.set('useFindAndModify', false)
-  
+
 module.exports = mongoose.model('Person', personSchema)
